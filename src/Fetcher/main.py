ccbbb8dd88aa14a58df830c150b5b234abd3f79c
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2023-06-17 16:14:35 -0700

    Implemented extract_data and added more TODO comments.

--- src/Fetcher/main.py
+++ src/Fetcher/main.py
@@ -2,11 +2,10 @@ import pika
 import requests
 
 
-
 def fetchData(subreddit: str, nsfw_allowed: bool = False) -> None:
     reddit_api = f"https://www.reddit.com/r/{subreddit}.json?sort=top?t=day"
 
-    #  Issue could be here if reddit bans this user agent. Fix is to simply change the version portion of the string.
+    #  TODO: Issue could be here if reddit bans this user agent. Fix is to simply change the version portion of the string.
     headers = {'User-agent': 'Youtube_Video_Automator/0.0.1'}
 
     response = requests.get(reddit_api, headers=headers)
@@ -18,12 +17,21 @@ def fetchData(subreddit: str, nsfw_allowed: bool = False) -> None:
         print(f"Error fetching data. Response code is {response.status_code}")
 
 
-def extract_data(datasrc, post_count = 1, desired_data=None):
+def extract_data(datasrc, post_count = 1, desired_data=None, nswf_allowed: bool = False):
     if desired_data is None:
-        desired_data = ['title', 'url', 'is_video', 'score', 'num_comments', 'view_count', 'ups', 'downs', 'selftext']
+        desired_data = ['title', 'url', 'is_video', 'score', 'num_comments', 'view_count', 'ups', 'downs',
+                        'selftext', 'over_18', 'author_fullname']
+
+    data = {}
+
     for i in range(0, post_count):
         for e in desired_data:
+            if e == 'over_18' and not nswf_allowed:
+                continue
             print(f"{e}: {datasrc['data']['children'][i]['data'][e]}")
+            data[e] = datasrc['data']['children'][i]['data'][e]
+
+    sendData(data)
 
 
 def sendData(data: dict) -> None:
@@ -33,7 +41,7 @@ def sendData(data: dict) -> None:
     channel.queue_declare(queue="imageWorker")
     channel.queue_declare(queue="audioWorker")
 
-    #  Might need to serialize dict data prior to sending to queues
+    #  TODO: Might need to serialize dict data prior to sending to queues
     channel.basic_publish(exchange='', routing_key='imageWorker', body=data)
     channel.basic_publish(exchange='', routing_key='audioWorker', body=data)
     #  Close conn.
