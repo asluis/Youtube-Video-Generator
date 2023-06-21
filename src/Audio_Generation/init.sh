b3b633e16524d654e8ff363ad6827fb51ed6f3d2
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2023-06-21 01:22:44 -0700

    Created requirements.txt and also updated the initialize bash script for the audio worker.

--- src/Audio_Generation/init.sh
+++ src/Audio_Generation/init.sh
@@ -6,9 +6,9 @@ set -e
 
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install python3.8
+sudo apt-get install python3.10
 sudo apt install python3-pip
 sudo pip3 install -r requirements.txt
 sudo chmod +x main.py
-sudo python3.8 main.py
+sudo python3.10 main.py
 echo "Successfully initialized Audio Worker"
