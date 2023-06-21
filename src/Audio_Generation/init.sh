7b3b6eb78b966c7e99f4896908c355ed5ef0cc66
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2023-06-21 01:12:36 -0700

    Created a failsafe consumption function to re-establish connection w/ queue after it loses connection after successfully processing the audio. The audioWorker now successfully processes audio too.

---
+++ src/Audio_Generation/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Assumes init.sh has root privileges
+
+set -e
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.8
+sudo apt install python3-pip
+sudo pip3 install -r requirements.txt
+sudo chmod +x main.py
+sudo python3.8 main.py
+echo "Successfully initialized Audio Worker"
--- src/Audio_Generation/main.py
+++ src/Audio_Generation/main.py
@@ -1,15 +1,19 @@
-from TTS.api import TTS
+from TTS.api import TTS  # Use Python verison 3.10 for this to work. Also use pip3 to install packages seen here
 from datetime import datetime
 import pika
+import json
+import time
 
 '''
 This code uses coqui-ai/TTS from github: https://github.com/coqui-ai/TTS
 
-It has many models available that change how the voice sounds. All are female.
+It has many models available that change how the voice sounds.
 '''
 
 def generateAudio(ch, method, properties, body) -> None:
-    data = body.decode()
+    data = json.loads(body.decode())
+    print(f"type of data is {type(data)}")
+    print(f"Data decoded: {data}")
     print(f"Picked up data. Self text is: {data['selftext']}")
     model_name = 'tts_models/en/ek1/tacotron2'
 
@@ -20,16 +24,35 @@ def generateAudio(ch, method, properties, body) -> None:
     
     Example: bobby11_47_30.wav
     '''
-    file_name = f"{data['author_fullname']}{datetime.now().strftime('%H_%M_%S')}"
+    file_name = f"{data['author_fullname']}{datetime.now().strftime('%H_%M_%S')}.mp3"
+    print(f"File name is {file_name}")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
     tts.tts_to_file(text=data['selftext'], file_path=file_name)
+    print(f"Done creating {file_name}")
 
-
-if __name__ == '__main__':
+def consume_messages():
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 
     channel = connection.channel()
 
     channel.queue_declare('audioWorker')
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='audioWorker', on_message_callback=generateAudio, auto_ack=False)
+    print("Starting to consume...")
+    try:
+        channel.start_consuming()
+    except pika.exceptions.StreamLostError:
+        # Connection was lost
+        # Attempt reconnection after a delay
+        print("Connection lost. Reconnecting...")
+        time.sleep(5)  # Delay before attempting reconnection
+        consume_messages()  # Recursively call the function to reconnect
+    except KeyboardInterrupt:
+        channel.stop_consuming()
 
-    channel.basic_consume(queue='audioWorker', on_message_callback=generateAudio(), auto_ack=True)
-    channel.start_consuming()
+    connection.close()
+
+
+if __name__ == '__main__':
+    consume_messages()
