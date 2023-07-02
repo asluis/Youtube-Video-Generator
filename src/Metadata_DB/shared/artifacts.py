5786f3ea693c3950beb62f3d25fd19e12405d269
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2023-07-02 15:23:11 -0700

    Added MySQL connection string.

--- src/Metadata_DB/shared/artifacts.py
+++ src/Metadata_DB/shared/artifacts.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine as db
 from sqlalchemy.orm import sessionmaker
 
-engine = db.create_engine('DB CONNECTION STRING HERE')
+# Username = metadata, password = root, database name = MetaData
+engine = db.create_engine('mysql://metadata:root@localhost/Metadata')
 session = sessionmaker(bind=engine)
