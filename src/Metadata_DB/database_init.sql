16063a310249baa29a95a7f1fdaf959116a6d8ff
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2023-07-03 14:42:15 -0700

    Added comment explaining why table was not created in init_db.sql script. Also amde the newly created user's privileges more restricted to only the Metadata database.

--- src/Metadata_DB/database_init.sql
+++ src/Metadata_DB/database_init.sql
@@ -1,5 +1,7 @@
-CREATE USER IF NOT EXISTS 'metadata'@'localhost' IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON *.* TO 'metadata'@'localhost'; -- For simplicity reasons we are granting user all privileges.
-FLUSH PRIVILEGES;
-
 CREATE DATABASE IF NOT EXISTS Metadata;
+USE Metadata;
+-- Leaving the table creation logic to sqlalchemy
+
+CREATE USER IF NOT EXISTS 'metadata'@'localhost' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON Metadata.* TO 'metadata'@'localhost'; -- For simplicity reasons we are granting all privileges
+FLUSH PRIVILEGES;
