c4abc42f9b84fedb27a9b5feb3600ecfed22cca4
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2023-07-04 21:04:25 -0700

    Fixed bug where create_engine was being called incorrectly.

--- src/Metadata_DB/shared/artifacts.py
+++ src/Metadata_DB/shared/artifacts.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine as db
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Username = metadata, password = root, database name = MetaData
-engine = db.create_engine('mysql://metadata:root@localhost/Metadata')
+engine = create_engine('mysql://metadata:root@localhost/Metadata')
 session = sessionmaker(bind=engine)
